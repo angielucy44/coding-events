f30e477067e4a75b479c81f7fd790346613a897a
Angie Zarinelli <angie.zarinelli@gmail.com>
2022-10-02 18:24:35 -0500

    13.7 complete

--- coding-events/src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ coding-events/src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -3,7 +3,9 @@ package org.launchcode.codingevents.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,21 +13,27 @@ import java.util.List;
 @Controller
 @RequestMapping("events")
 public class EventController {
+
+    private static List<String> events = new ArrayList<>();
+
     @GetMapping
     public String displayAllEvents(Model model) {
-        List<String> events = new ArrayList<>();
-        events.add("Code With Pride");
-        events.add("Strange Loop");
-        events.add("Apple WWDC");
-        events.add("SpringOne Platform");
-        model.addAttribute("events", events);
+       model.addAttribute("events", events);
         return "events/index";
     }
 
-    //lives at /events/create
+    //lives at /events/create - can be same as below b/c diff type of request- this one is GET
     @GetMapping("create")
     public String renderCreateEventForm() {
         return "events/create";
     }
+//    lives at events/create - can be same as above b/c diff type of request - this one is POST
+    @PostMapping("create")
+    public String createEvent(@RequestParam String eventName) {
+        events.add(eventName);
+        return "redirect:";
+
+    }
 }
+
 
